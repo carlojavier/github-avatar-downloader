c37a084374879193994b5aca9e9e6f0a19041fee
Carlo <carlomjavier@gmail.com>
2019-06-04 01:39:57 +0000

    implemented function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
+var https = require('https');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    // ...
+    // request('https://github.com/nodejs/node', function(repoOwner, repoName, cb) {
+    //     if (err) throw err;
+
+    //     console.log('Response Status Code:', response.statusCode);
+
+
+
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
