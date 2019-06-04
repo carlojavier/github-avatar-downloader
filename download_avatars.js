980b1526c8d69842b364156ceafb7fb583b8c9c1
Carlo <carlomjavier@gmail.com>
2019-06-04 02:25:10 +0000

    parsed the thing

--- download_avatars.js
+++ download_avatars.js
@@ -7,9 +7,9 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
     // ...
     var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + "/contributors",
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
-            'User-Agent': 'request'
+            'User-Agent': 'request',
             'Authorization': "token " + secrets
         }
     };
@@ -21,5 +21,5 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    console.log("Result:", JSON.parse(result));
 });
